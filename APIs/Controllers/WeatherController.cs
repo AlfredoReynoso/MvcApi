d9b3c3dee52295dceae9c764fbdfc7b914c9e05b
Alfredo Reynoso <alfredoreynoso@gmail.com>
2025-10-16 13:18:18 -0600

    Se agrega el controlador Weather, se agrega la carpeta ViewModel y el WeatherModel, se modifica el _Layout se agrega la vista index, estas cargas son necesaria para el proceso de conexion api con una solicitud para obtener el clima de una ciudad

---
+++ APIs/Controllers/WeatherController.cs
@@ -0,0 +1,53 @@
+﻿using APIs.Models.ViewModel;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APIs.Controllers
+{
+    public class WeatherController : Controller
+    {
+
+        private static HttpClient client = new HttpClient();
+        private string apiKey = "5546c71651754323e17a1a660279a933";
+
+        public ActionResult Index ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(string Name)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                ViewBag.Mensaje = "Por favor ingrese una ciudad";
+                return View();
+            }
+
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={Name}&appid={apiKey}&units=metric";
+
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+            if(response.IsSuccessStatusCode)
+            {
+                string jsonData = await response.Content.ReadAsStringAsync();
+                var weatherData = JsonConvert.DeserializeObject<WeatherModel>(jsonData);
+                return View(weatherData);
+            }
+            else
+            {
+                ViewBag.Mensaje = "No se encontró la ciudad o ocurrió un error.";
+                return View();
+            }
+
+        }
+
+    }
+}
---
+++ APIs/Models/ViewModel/WeatherModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIs.Models.ViewModel
+{
+    public class WeatherModel
+    {
+        public string Name { get; set; }
+        public MainInfo Main { get; set; }
+        public List<WeatherInfo> Weather { get; set; }
+
+        public class MainInfo
+        {
+            public double Temp { get; set; }
+            public double Humidity { get; set; }
+        }
+
+        public class WeatherInfo
+        {
+            public string Main {  get; set; }
+            public string Description { get; set; }
+
+            public string Icon { get; set; }
+        }
+
+
+    }
+}
